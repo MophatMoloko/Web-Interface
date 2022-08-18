adcab1dbb27d873c2d7456de3aff7a8f92fc68bc
Moffat <mophatmoloko@gmail.com>
2022-08-18 14:39:32 +0200

    final commit for demo

--- webinterface/incidents/forms.py
+++ webinterface/incidents/forms.py
@@ -1,5 +1,6 @@
 from dataclasses import fields
 from socket import fromshare
+from tkinter import Widget
 from django import forms
 from .models import Incidents
 
@@ -11,8 +12,8 @@ class IncidentsForm(forms.ModelForm):
             'text': 'Write accompanying message for the request:'
         }
 
-    def clean_title(self):
-        title = self.cleaned_data['title']
-        if 'Convener' not in title:
-            raise ValueError("We only accept incidents for the Converner")
-        return title
+    #def clean_title(self):
+     #   title = self.cleaned_data['title']
+      #  if 'Convener' not in title:
+       #     raise ValueError("We only accept incidents for the Converner")
+        #return title
