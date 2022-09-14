39bd95e7b7538cf1b0d7ab53ce06f719ef158f9a
Ilyaas Toefy <tfyily001@myuct.ac.za>
2022-09-14 21:37:10 +0200

    Updated views and templates

--- webinterface/Tutor_Marking/urls.py
+++ webinterface/Tutor_Marking/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('Tutor_Marking',views.ListTutorMarking.as_view(), name="Tutor_Markers"),
+    path('Tutor_Marking',views.ListTutorMarking.as_view(), name="Tutor_List"),
+    path('tutor_upload', views.simple_uploads, name = "uploads")
 ]
--- webinterface/Tutor_Marking/views.py
+++ webinterface/Tutor_Marking/views.py
@@ -5,16 +5,41 @@ from django.views.generic import ListView, DetailView
 from webinterface.settings import BASE_DIR
 from .models import Tutor_Marking
 from xlsx2html import xlsx2html 
+from tablib import Dataset
+from django.contrib import messages
 
 
 
 # Create your views here.
 
 #Gives a list of all the tutors for each assignment marked
+def simple_uploads(request):
+
+    if request.method == "POST":
+        dataset = Dataset()
+        new_student = request.FILES['myfile']
+
+        if not new_student.name.endswith('xlsx'):
+            messages.info(request, "Wrong format")
+            return render(request,'Tutor_Marking/markers.html')
+
+        imported_data = dataset.load(new_student.read(),format='xlsx')
+        for data in imported_data:
+            value = Tutor_Marking(data[0],
+                            data[1],
+                            data[2],
+                            data[3],
+                            data[4],
+                            data[5],
+                            data[6],
+                            )
+            value.save()
+    return render(request, 'Tutor_Marking/markers.html')
+
 class ListTutorMarking(ListView):
     model = Tutor_Marking
     context_object_name ="students"
-    template_name = 'Tutor_Marking/markers.html'
+    template_name = 'Tutor_Marking/tutor_marking_list'
 
     def get_queryset(self):
         return Tutor_Marking.objects.all()
